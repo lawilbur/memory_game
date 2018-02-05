89d667d90641e63d2613dac690547f93669fe11a
Alec Wilbur <lawilbur2@gmail.com>
2018-02-05 12:02:14 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-var c1 = "queen";
-var c2 = "queen";
-var c3 = "king";
-var c4 = "king";
 
-console.log("User flipped " + c1);
-console.log("hey")
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
+}
